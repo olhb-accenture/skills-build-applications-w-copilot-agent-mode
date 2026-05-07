c3f691b8a4d97f51f38ef00beb745a017612fc5e
olhb-accenture <oscar.hermoso.blanco@accenture.com>
2026-05-07 13:40:30 +0000

    Refactor populate_db.py to set up the Django environment with BASE_DIR and sys.path adjustments

--- octofit-tracker/backend/populate_db.py
+++ octofit-tracker/backend/populate_db.py
@@ -1,5 +1,10 @@
 import django
 import os
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+sys.path.append(str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'octofit_tracker.settings')
 django.setup()
